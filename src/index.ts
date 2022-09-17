63efecd0e2295e4628e0889c1d346e52fa453e0e
tigerabrodi <tigerabrodi@gmail.com>
2022-09-17 07:31:39 +0200

    does string for visitorId work

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
 // Load the AWS SDK for Node.js
 import AWS from "aws-sdk";
-const dynamo = new AWS.DynamoDB.DocumentClient();
+const documentClient = new AWS.DynamoDB.DocumentClient({
+  region: "eu-west-3",
+});
 
 export const handler = async () => {
-  return await dynamo
+  return await documentClient
     .get({
       TableName: "visitors",
       Key: {
-        visitorId: 1,
+        visitorId: "1",
       },
     })
     .promise();
