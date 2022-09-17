770e0048edaae50dd5956d20ae4c7538e0cd2488
tigerabrodi <tigerabrodi@gmail.com>
2022-09-17 06:29:32 +0200

    return visitor count

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
-export const handler = async () => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify(1),
-  };
+// Load the AWS SDK for Node.js
+import AWS from "aws-sdk";
+const dynamo = new AWS.DynamoDB.DocumentClient();
 
-  return response;
+export const handler = async () => {
+  return await dynamo
+    .get({
+      TableName: "visitors",
+      Key: {
+        visitorId: 1,
+      },
+    })
+    .promise();
 };
